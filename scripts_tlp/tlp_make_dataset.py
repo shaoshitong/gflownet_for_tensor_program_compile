27a1aad99c32f3e1850aea9fe59d4ed804233f1f
shaoshitong <1090784053@qq.com>
2023-07-01 15:37:50 +0800

    update dataset collect

--- scripts_tlp/tlp_make_dataset.py
+++ scripts_tlp/tlp_make_dataset.py
@@ -12,13 +12,13 @@ from tvm.tir.expr import FloatImm
 import numpy as np
 import random
 import argparse
+import json
 
 def handle_file(file_idx, file):
     print(file_idx) # 打印当前处理的文件索引
 
     with open(file, 'r') as f: # 以只读方式打开文件
         lines = f.read().strip().split('\n') # 读取文件内容，并移除前后的空格和换行符，然后按行分割成列表
-
     inputs, outputs = auto_scheduler.RecordReader(file).read_lines() # 使用TVM auto_scheduler的RecordReader读取文件中的记录
     task = auto_scheduler.measure.recover_measure_input(inputs[0]).task # 使用第一行输入数据恢复度量输入，获取其任务信息
 
@@ -50,12 +50,45 @@ def handle_file(file_idx, file):
                 elif isinstance(it, str): # 如果元素是字符串，根据不同的条件，将相应的结果追加到vec中
                     if st[0] == 'PR' and 'auto_unroll_max_step' in it:
                         vec.append(auto_unroll_max_step_to_idx[it])
-                    elif st
+                    elif st[0] == 'CHW':
+                        vec.append(chw_dict[it])
+                    elif st[0] == 'CHR' and it == 'shared':
+                        vec.append(1)
+                    else: # 如果元素不满足任何条件，断言错误
+                        assert False
+                else: # 如果元素既不是整型，也不是列表，也不是字符串，断言错误
+                    assert False
+
+            assert len(vec) <= max_emb_size # 断言vec的长度小于等于最大嵌入大小
+            for i in range(len(vec), max_emb_size, 1): # 如果vec的长度小于最大嵌入大小，将0追加到vec中，直到长度等于最大嵌入大小
+                vec.append(0)
+
+            vec = vec[:args.crop_emb_size] # 对vec进行切片，取出前crop_emb_size个元素
+            step_vecs.append(vec) # 将vec追加到step_vecs中
+
+        assert len(step_vecs) <= max_seq_len # 断言step_vecs的长度小于等于最大序列长度
+        vec = [0] * args.crop_emb_size # 初始化vec，长度为crop_emb_size，每个元素都是0
+        for i in range(len(step_vecs), max_seq_len, 1): # 如果step_vecs的长度小于最大序列长度，将vec复制并追加到step_vecs中，直到长度等于最大序列长度
+            step_vecs.append(vec.copy())
+        step_vecs = step_vecs[:args.crop_seq_len] # 对step_vecs进行切片，取出前crop_seq_len个元素
+
+        costs = [x.value for x in outputs[line_idx].costs if isinstance(x, FloatImm)] # 从outputs中提取costs的值，只提取类型为FloatImm的元素
+        cost = np.mean(costs) # 计算costs的平均值
+        line_vecs.append((step_vecs, cost)) # 将step_vecs和cost作为元组追加到line_vecs中
+        min_cost = min(min_cost, cost) # 更新最小代价值
+    line_vecs_new = [] # 初始化line_vecs_new列表，用于存储新的line_vecs
+    for line_vec in line_vecs: # 遍历line_vecs中的每个元素
+        step_vecs, cost = line_vec # 解包每个元素，得到step_vecs和cost
+        score = min_cost / cost # 计算得分，最小代价值除以当前的cost
+        line_vecs_new.append((step_vecs, score, min_cost)) # 将step_vecs，得分和最小代价值作为元组追加到line_vecs_new中
+    line_vecs = line_vecs_new # 更新line_vecs为line_vecs_new
+
+    return (file, file_idx, workloadkey_idx, task.workload_key, workload_args, task.compute_dag.flop_ct, line_vecs) # 返回结果，包括文件名、文件索引、workloadkey索引、workload_key、workload参数、计算DAG的浮点操作计数和line_vecs
 
 
 
 def make_all_dataset(json_files_path):
-    tasks = load_and_register_tasks()
+    # tasks = load_and_register_tasks()
     json_files = sorted(glob.glob(json_files_path + '/' + '*.json'))
     json_files = random.sample(json_files, args.files_cnt)
 
@@ -121,7 +154,7 @@ set_seed(0)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--json_files_path", type=str, default='dataset/measure_records/platinum-8272')
+    parser.add_argument("--json_files_path", type=str, default='dataset_cpu/measure_records/e5-2673-v2')
     parser.add_argument("--files_cnt", type=int, default=2308)
     parser.add_argument("--save_name", type=str, default='')
     parser.add_argument("--platform", type=str, default='llvm')  # or cuda
@@ -134,17 +167,17 @@ if __name__ == "__main__":
 
     hold_out_tasks = []
     files = [
-        'dataset/network_info/((resnet_50,[(1,3,224,224)]),%s).task.pkl',
-        'dataset/network_info/((mobilenet_v2,[(1,3,224,224)]),%s).task.pkl',
-        'dataset/network_info/((resnext_50,[(1,3,224,224)]),%s).task.pkl',
-        'dataset/network_info/((bert_base,[(1,128)]),%s).task.pkl',
-        'dataset/network_info/((bert_tiny,[(1,128)]),%s).task.pkl'
+        'dataset_cpu/network_info/((resnet_50,[(1,3,224,224)]),%s).task.pkl',
+        'dataset_cpu/network_info/((mobilenet_v2,[(1,3,224,224)]),%s).task.pkl',
+        'dataset_cpu/network_info/((resnext_50,[(1,3,224,224)]),%s).task.pkl',
+        'dataset_cpu/network_info/((bert_base,[(1,128)]),%s).task.pkl',
+        'dataset_cpu/network_info/((bert_tiny,[(1,128)]),%s).task.pkl'
     ]
-    for file in files:
-        tasks_part, task_weights = pickle.load(open(file % args.platform, "rb"))
-        hold_out_tasks.extend(tasks_part)
+    # for file in files:
+    #     tasks_part, task_weights = pickle.load(open(file % args.platform, "rb"))
+    #     hold_out_tasks.extend(tasks_part)
 
-    hold_out_tasks_set = set([task.workload_key for task in hold_out_tasks])
+    # hold_out_tasks_set = set([task.workload_key for task in hold_out_tasks])
 
     with open('tlp_make_dataset_str_to_idx_%s.pkl' % args.platform, 'rb') as f:
         workloadkey_to_index, stepname_to_idx, auto_unroll_max_step_to_idx = pickle.load(f)
